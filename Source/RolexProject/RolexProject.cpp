9af576d39fadd55c74d0085855329f8d44953c6f
SeungWooChoi <89066816+Seawo@users.noreply.github.com>
2024-12-16 16:21:18 +0900

    first commit

---
+++ Source/RolexProject/RolexProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RolexProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RolexProject, "RolexProject" );
+ 
---
+++ Source/RolexProject/RolexProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RolexProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARolexProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARolexProjectGameMode();
+};
+
+
+
