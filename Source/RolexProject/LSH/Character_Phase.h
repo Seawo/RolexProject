3167d82c3beb4f3bc15701f74b35ddd9e85648b4
VecTor <lsh961101@naver.com>
2024-12-18 12:43:58 +0900

    [추가] 언리얼 내 폴더 생성 및 Phase 생성

---
+++ Source/RolexProject/LSH/Character_Phase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "Character_Phase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLEXPROJECT_API ACharacter_Phase : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
