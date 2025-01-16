60d48b3a11329efa06012e726d379e5107ed7160
VecTor <lsh961101@naver.com>
2025-01-16 16:32:29 +0900

    [추가] 힐팩

---
+++ Source/RolexProject/LSH/Actor_HealPack.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LSH/Actor_HealPack.h"
+
+#include "Components/BoxComponent.h"
+#include "NiagaraComponent.h"
+#include "Actor_HealPack.h"
+#include "BaseCharacter.h"
+
+
+// Sets default values
+AActor_HealPack::AActor_HealPack()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ColComp = CreateDefaultSubobject<UBoxComponent>(TEXT("ColComp"));
+	SetRootComponent(ColComp);
+
+	NiagaraComponentHealPack = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraComponentHealPack"));
+	NiagaraComponentHealPack->SetupAttachment(ColComp);
+}
+
+// Called when the game starts or when spawned
+void AActor_HealPack::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ColComp->OnComponentBeginOverlap.AddDynamic(this, &AActor_HealPack::OnOverlapBegin);
+}
+
+// Called every frame
+void AActor_HealPack::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AActor_HealPack::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (not bIsActivated) return;
+
+
+	UE_LOG(LogTemp, Warning, TEXT("HealPack Overlap Begin"));
+	ABaseCharacter* character = Cast<ABaseCharacter>(OtherActor);
+
+	if (character)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HealPack Overlap Begin2"));
+
+		character->ModifyHP(3);
+		NiagaraComponentHealPack->SetVisibility(false);
+		bIsActivated = false;
+	}
+
+	// 2초 후에 다시 활성화
+	FTimerHandle timerHandle;
+	GetWorld()->GetTimerManager().SetTimer(timerHandle, [this]()
+		{
+			NiagaraComponentHealPack->SetVisibility(true);
+			bIsActivated = true;
+		}, 5.0f, false);
+}
+
---
+++ Source/RolexProject/LSH/Actor_HealPack.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Actor_HealPack.generated.h"
+
+UCLASS()
+class ROLEXPROJECT_API AActor_HealPack : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AActor_HealPack();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Effect")
+	class UNiagaraComponent* NiagaraComponentHealPack;
+
+	UPROPERTY(EditAnywhere, Category = "Effect")
+	class UBoxComponent* ColComp;
+
+
+	bool bIsActivated = true;
+};
