91f3bbc692cae08d6df40aecb7d8913a6676522e
jaeun <everlo5>
2025-01-19 17:08:58 +0900

    feat: Hero widget

---
+++ Source/RolexProject/PJE/Private/HeroUI.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HeroUI.h"
+
+void UHeroUI::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+}
---
+++ Source/RolexProject/PJE/Public/HeroUI.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HeroUI.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ROLEXPROJECT_API UHeroUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+	virtual void NativeConstruct() override;
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class ABaseCharacter* BaseCharacter;
+	
+	// HP
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* HP;
+	
+	// Max HP
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* MaxHP;
+
+	// HP Bar
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HPBar;
+	
+	// Hero Image
+
+	// Skill Image
+	UPROPERTY(meta = (BindWidget))
+	class UImage* SkillImage;
+	
+	// Skill CoolTime
+	int32 CoolTime;
+
+	// Ammo
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* Ammo;
+	
+	// MaxAmmo
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* MaxAmmo;
+	
+	// Weapon Image
+	UPROPERTY(meta = (BindWidget))
+	UImage* WeaponImage;
+	
+	
+};
