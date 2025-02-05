3b27652d65899535587dc7aa88a5cf8bceeb482f
VecTor <lsh961101@naver.com>
2025-02-05 14:54:47 +0900

    [수정] 캐릭터 위치 생성 및 팀 선택

--- Source/RolexProject/RolexGameMode.cpp
+++ Source/RolexProject/RolexGameMode.cpp
@@ -30,60 +30,6 @@ void ARolexGameMode::BeginPlay()
 {
 	UE_LOG(LogTemp, Warning, TEXT("------------ RolexGameMode BeginPlay ------------"));
 	Super::BeginPlay();
-	
-	//FTimerHandle timer;
-	//GetWorldTimerManager().SetTimer(timer, [this]()
-	//{
-	//	UE_LOG(LogTemp, Warning, TEXT("[GameMode BeginPlay] GS_TrainingRoom is valid"));
-	//	TArray<AActor*> foundActors;
-	//	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABaseCharacter::StaticClass(), foundActors);
-
-	//	for (AActor* actor : foundActors)
-	//	{
-	//		ABaseCharacter* baseCharacter = Cast<ABaseCharacter>(actor);
-	//		if (baseCharacter)
-	//		{
-	//			baseCharacter->bTabTimer = true;
-	//			if (ARolexPlayerController* rolexPC = Cast<ARolexPlayerController>(baseCharacter->GetController()))
-	//			{
-	//				UE_LOG(LogTemp, Error, TEXT("[AGM_TrainingRoom] FindCharacterInWorld client PC : %p"), rolexPC);
-	//			}
-
-	//			if (baseCharacter->Data.Team)
-	//			{
-	//				ATeamChracters.Add(baseCharacter);
-
-	//			}
-	//			else
-	//			{
-	//				BTeamChracters.Add(baseCharacter);
-	//			}
-
-	//		}
-	//	}
-	//}, 5.0f, false);
-	
-
-	
-
-	/*
-	// print player name and team
-	FTimerHandle timerPlayer;
-	GetWorldTimerManager().SetTimer(timerPlayer, [this]()
-	{
-			TArray<AActor*> PlayerArray;
-			UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABaseCharacter::StaticClass(), PlayerArray);
-			for (AActor* Player : PlayerArray)
-			{
-				ABaseCharacter* BaseCharacter = Cast<ABaseCharacter>(Player);
-				if (BaseCharacter)
-				{
-					// ARolexPlayerState* RolexPlayerState = Cast<ARolexPlayerState>(BaseCharacter->GetPlayerState());
-					//BaseCharacter->Data.Team = RolexPlayerState->Team;
-					UE_LOG(LogTemp, Warning, TEXT("[GameMode BeginPlay] Name : %s, Team : %s"), *BaseCharacter->GetName(), BaseCharacter->Data.Team ? TEXT("ATeam") : TEXT("BTeam"));
-				}
-			}
-	}, 3.0f, false);*/
 }
 
 void ARolexGameMode::Tick(float DeltaTime)
@@ -104,6 +50,8 @@ UClass* ARolexGameMode::GetDefaultPawnClassForController_Implementation(AControl
 		RolexPlayerState->FindUniqueID();
 		if (TSubclassOf<ABaseCharacter>* BaseCharacterFactory = RolexGameInstance->PlayerHeroSelections.Find(RolexPlayerState->UniqueID))
 		{
+			BaseCharacterFactory->GetDefaultObject()->Data.Team = RolexGameInstance->PlayerTeam[RolexPlayerState->UniqueID];
+
 			UE_LOG(LogTemp, Warning, TEXT("[GameMode GetDefaultPawnClassForController] Player ID : %s"), *RolexPlayerState->UniqueID);
 			return *BaseCharacterFactory;	
 		}
@@ -112,6 +60,45 @@ UClass* ARolexGameMode::GetDefaultPawnClassForController_Implementation(AControl
 	UE_LOG(LogTemp, Warning, TEXT("Failed to find BaseCharacter"));
 	return Super::GetDefaultPawnClassForController_Implementation(InController);
 }
+
+APawn* ARolexGameMode::SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot)
+{
+	return GetWorld()->SpawnActor<ABaseCharacter>(GetDefaultPawnClassForController(NewPlayer),
+													StartSpot->GetActorLocation(), 
+													StartSpot->GetActorRotation());
+}
+
+AActor* ARolexGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	TArray<AActor*> PlayerStartArray;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), PlayerStartArray);
+
+	for (AActor* playerStart : PlayerStartArray)
+	{
+		ABaseCharacter* character = Cast<ABaseCharacter>(Player->GetPawn());
+		if (character->Data.Team)
+		{
+			if (playerStart->GetName().Contains(TEXT("PlayerStart_4")))
+			{
+				return playerStart;
+			}
+		}
+		else
+		{
+			if (playerStart->GetName().Contains(TEXT("PlayerStart_3")))
+			{
+				return playerStart;
+			}
+		}
+	}
+
+
+
+
+	return Super::ChoosePlayerStart_Implementation(Player);
+}
+
+
 
 void ARolexGameMode::PostLogin(APlayerController* NewPlayer)
 {
